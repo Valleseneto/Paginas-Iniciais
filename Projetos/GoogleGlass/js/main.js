64abf66de24b55b663b4ac64abe7bc070c7ccd75
Valleseneto <tinovallese@gmail.com>
2021-07-07 13:56:58 -0300

    Google Glass Guanabara

---
+++ Projetos/GoogleGlass/js/main.js
@@ -0,0 +1,5 @@
+//document.write serve para escrever no documento ("")
+//alert gera um alerta na tela e isso muda de acordo com cada navegador
+function mudaFoto (foto){
+    document.getElementById("icone").src = foto;
+}
